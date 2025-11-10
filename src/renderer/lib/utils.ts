7ebf2c84483a052ccf6d706ac4538437f714b91e
Chamindu Vidyarathne <chamindudvidyarathne@gmail.com>
2025-11-11 00:15:43 +0530

    feature: Added paths and utils for playing cards

--- src/renderer/lib/utils.ts
+++ src/renderer/lib/utils.ts
@@ -1,6 +1,18 @@
 import { type ClassValue, clsx } from 'clsx';
+import { ASSETS } from 'renderer/assetPaths';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export type Suit = 'CLUBS' | 'DIAMONDS' | 'HEARTS' | 'SPADES';
+export type Rank = '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K' | 'A';
+
+export function getCardImage(suit: Suit, rank: Rank): string {
+  return ASSETS.CARDS[suit][rank];
+}
+
+export function getSuitCards(suit: Suit) {
+  return ASSETS.CARDS[suit];
 }
